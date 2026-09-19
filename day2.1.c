9e7d3f00079281e84e4472b992bf161456b1b2a8
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-19 23:11:02 +0530

    Add files via upload

---
+++ day2.1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int length, breadth;
+    
+    scanf("%d %d", &length, &breadth);
+    
+    printf("Area=%d, Perimeter=%d",
+           length * breadth,
+           2 * (length + breadth));
+    
+    return 0;
+}
