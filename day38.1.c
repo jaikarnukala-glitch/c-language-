2504deaf4abf8bd685288ffdd1fcd1384aa396d0
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-20 18:36:16 +0530

    Add files via upload

---
+++ day38.1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+    int r1, c1, r2, c2;
+    int a[100][100], b[100][100];
+
+    scanf("%d %d", &r1, &c1);
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    scanf("%d %d", &r2, &c2);
+
+    for (int i = 0; i < r2; i++) {
+        for (int j = 0; j < c2; j++) {
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    if (r1 != r2 || c1 != c2) {
+        printf("Addition not possible");
+        return 0;
+    }
+
+    for (int i = 0; i < r1; i++) {
+        for (int j = 0; j < c1; j++) {
+            printf("%d%s", a[i][j] + b[i][j], j == c1 - 1 ? "" : " ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
