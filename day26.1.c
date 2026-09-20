55400534dbaf8a37f8823373e07bc46e4aea8d6f
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-20 17:18:44 +0530

    Add files via upload

---
+++ day26.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, spaces;
+
+    for (i = 5; i >= 1; i--) {
+        for (spaces = 1; spaces < i; spaces++) {
+            printf(" ");
+        }
+
+        for (j = i; j <= 5; j++) {
+            printf("%d", j);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
