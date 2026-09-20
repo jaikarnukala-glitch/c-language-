61b374e6ebe57e9ae6deb68891531fa2a8e0369f
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-20 18:30:51 +0530

    Add files via upload

---
+++ day36.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int rows, cols, matrix[100][100];
+    int sum = 0;
+
+    scanf("%d %d", &rows, &cols);
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            scanf("%d", &matrix[i][j]);
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
