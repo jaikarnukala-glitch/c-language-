a2691f6200ea5ce280366ecf45ea0aaa66e23383
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-20 16:57:11 +0530

    Add files via upload

---
+++ day20.2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    char binary[100];
+    int i = 0;
+
+    scanf("%s", binary);
+
+    while (binary[i] != '\0') {
+        if (binary[i] == '0') {
+            binary[i] = '1';
+        } else if (binary[i] == '1') {
+            binary[i] = '0';
+        }
+
+        i++;
+    }
+
+    printf("%s", binary);
+
+    return 0;
+}
