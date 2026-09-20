4b8366835e8685a15c4bd9e1ddde9bf4c789f689
jaikarnukala-glitch <jaikarnukala@gmail.com>
2026-09-20 15:45:17 +0530

    Add files via upload

---
+++ day6.2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    scanf("%d", &number);
+
+    if (number >= 0) {
+        if (number == 0) {
+            printf("Zero");
+        } else {
+            printf("Positive");
+        }
+    } else {
+        printf("Negative");
+    }
+
+    return 0;
+}
